dfb6a68371442d7e368d426f1bbfc544a2bcc526
jbucsa <justin.bucsa@gmail.com>
2024-01-29 14:18:28 -0600

    Ran npm install react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 
-
 const App = () => {
   return (
     <>
-      <div>App</div>
       <h1 className='text-3xl font-bold underline'>
           Hello World!!
       </h1>
