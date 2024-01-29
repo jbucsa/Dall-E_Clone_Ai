29cf2910c40a85b36d4d629e3128a37d49d205e1
jbucsa <justin.bucsa@gmail.com>
2024-01-29 17:52:17 -0600

    Created forms, useState, and handle function inside of CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -8,8 +8,28 @@ import { FormField, Loader } from '../components';
 
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg ] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7x-l mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w [500px]'>
+          Create collection of imaginative and visually stunning images through Dall-E AI and share them with the community!!
+        </p>
+      </div>
+    </section>
   )
 };
 
