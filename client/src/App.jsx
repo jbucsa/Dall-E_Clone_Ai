9ec506c9ffde8eb9ce79f8589d8d5772c54b3d19
jbucsa <justin.bucsa@gmail.com>
2024-01-29 15:07:30 -0600

    added the following files.associations: { *.css: tailwindcss} to package.json file to get Tailwind to work. Check code for proper formating.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from './assets';
+
+//This line is used to import the CreatePost and Home from the index.js file in the pages folder.
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>
-          Hello World!!
-      </h1>
+      <BrowserRouter>
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28 object-contain" />
+          </Link>
+        </header>
+      </BrowserRouter>
     </>
   )
 };
---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+//This file serves as the export file for CreatePost.jsx and Home.jsx
+
+import Home from './Home';
+import CreatePost from './CreatePost';
+
+export { 
+    Home,
+    CreatePost
+};
