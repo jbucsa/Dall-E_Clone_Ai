d361b3b10b13ad8f88c76de2121dd8d8f6c34191
jbucsa <justin.bucsa@gmail.com>
2024-01-29 17:52:49 -0600

    Created forms, useState, and handle function inside of CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,6 +18,11 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg ] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {};
+
+  const handleChange = (e) => {};
+
+  const handleSurpriseMe = () => {};
 
   return (
     <section className='max-w-7x-l mx-auto'>
@@ -29,6 +34,32 @@ const CreatePost = () => {
           Create collection of imaginative and visually stunning images through Dall-E AI and share them with the community!!
         </p>
       </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="promt"
+            placeholder="an oil pastel drawing of an annoyed cat in a spaceship"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+
+      </form>
+
+
     </section>
   )
 };
