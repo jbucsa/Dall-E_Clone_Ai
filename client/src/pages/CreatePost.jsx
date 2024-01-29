f458391b5f892c516b4ea8840f4cd71e6e7f3a81
jbucsa <justin.bucsa@gmail.com>
2024-01-29 15:43:59 -0600

    Begin Components development.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-function CreatePost() {
+const CreatePost = () => {
   return (
     <div>CreatePost</div>
   )
-}
+};
 
-export default CreatePost
+export default CreatePost;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-function Home() {
+const Home = () => {
   return (
     <div>Home</div>
   )
-}
+};
 
-export default Home
+export default Home;
