7c01deffefcb4d8953fd090760d747ce59dc4b4c
jbucsa <justin.bucsa@gmail.com>
2024-01-31 19:56:26 -0600

    OpenAi connected to front-end. Picture rendering functional.

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -19,7 +19,7 @@ router.route('/').post(async (req, res) =>{
     try {
         const { prompt } = req.body;
 
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
             prompt,
             n: 1,
             size: '1024x1024',
@@ -30,7 +30,7 @@ router.route('/').post(async (req, res) =>{
         res.status(200).json({ photo: image });
     } catch (error){
         console.log(error);
-        res.status(500).send(error?.response.data.error.message);
+        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
     }
 }); 
 
