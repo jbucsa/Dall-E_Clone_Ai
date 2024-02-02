46a3d3e9afe4796e23dff92e9b4bd4575f4c17aa
jbucsa <justin.bucsa@gmail.com>
2024-02-02 16:01:13 -0600

    NMP Run Buid upload.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://bucsa-ai-image-generator.onrender.com//api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
